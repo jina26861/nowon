adc9f59348be2b6dd0c92a68620f3f1f87e3a683
jina26861 <55438164+jina26861@users.noreply.github.com>
2020-08-16 02:33:56 +0900

    파이썬의  데이터표현

---
+++ 20200815.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello world!")
+
+
+# In[16]:
+
+
+a=10
+print(type(a))
+a
+
+
+# In[18]:
+
+
+b="abcc"
+print(type(b))
+b
+
+
+# In[19]:
+
+
+c=[10,3,2,4,4]
+print(type(c))
+c
+
+
+# In[20]:
+
+
+d=(1,2,3,4,4)
+print(type(d))
+d
+
+
+# In[21]:
+
+
+e={1,2,3,5,5}
+print(type(e))
+e
+
+
+# In[22]:
+
+
+f={"a":"apple","b":"banana"}
+print(type(f))
+f
+
+
+# In[ ]:
+
+
+
+
